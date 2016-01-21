3a3331797f31e54ddb613d108fe4e6290f093820
Oleg Kipling <oleg.kipling@gmail.com>
2016-01-21 18:01:41 +0300

    travis x

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ git config user.name "Travis CI"
 git config user.email "oleg.kipling@gmail.com>"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+# git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
