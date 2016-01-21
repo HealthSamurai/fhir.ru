c50c8ec9224f44a6264ea45ce6818ea1e57bea4a
Oleg Kipling <oleg.kipling@gmail.com>
2016-01-21 18:11:40 +0300

    travis x

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 cd ./build/
+touch ~/.ssh/travis_fhir_ru_deploy
+chmod 600 ~/.ssh/travis_fhir_ru_deploy
+echo -e "Host github.com\n  IdentityFile ~/.ssh/travis_fhir_ru_deploy" > ~/.ssh/config
 git init
 git config user.name "Travis CI"
-git config user.email "oleg.kipling@gmail.com>"
+git config user.email "robot@health-samurai.io"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet origin gh-pages
 # git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
